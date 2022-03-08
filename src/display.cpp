e850dbefed277f5e2d410917c288cd0a1e8f812c
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-03-08 11:22:07 +0100

    CMakeLists and src folder

---
+++ src/display.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lada on 3/8/22.
+//
+
+#include "display.h"
---
+++ src/display.h
@@ -0,0 +1,14 @@
+//
+// Created by lada on 3/8/22.
+//
+
+#ifndef SNAKES_DISPLAY_H
+#define SNAKES_DISPLAY_H
+
+
+class display {
+
+};
+
+
+#endif //SNAKES_DISPLAY_H
